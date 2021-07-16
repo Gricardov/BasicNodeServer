2435c7f7744d2a424f4c78f9de6f015938dea917
gricardov <gricardov@gmail.com>
2021-07-15 20:16:48 -0500

    basic class for server

---
+++ models/server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // Middlewares
+        this.middlewares();
+
+        // Rutas
+        this.routes();
+    }
+
+    middlewares() {
+        this.app.use(express.static('public'));
+    }
+
+    routes() {
+        this.app.get('/api', (req, res) => {
+            res.json({
+                ok: true,
+                msg: 'get'
+            })
+        })
+        this.app.put('/api', (req, res) => {
+            res.json({
+                ok: true,
+                msg: 'put'
+            })
+        })
+        this.app.post('/api', (req, res) => {
+            res.json({
+                ok: true,
+                msg: 'post'
+            })
+        })
+        this.app.delete('/api', (req, res) => {
+            res.json({
+                ok: true,
+                msg: 'dekete'
+            })
+        })
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Listening on ' + this.port);
+        })
+    }
+}
+
+module.exports = Server;
